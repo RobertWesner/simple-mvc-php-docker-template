6fa31ad311ee951528109aa1c1790d9f6eaa476c
RobertWesner <robert@wesner.io>
2025-05-14 17:41:42 +0200

    Improved welcome page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use RobertWesner\SimpleMvcPhp\Route;
 
 Route::get('/', function () {
     return Route::render('index.twig', [
-        'background' => '#' . dechex(mt_rand(0x999999, 0xFFFFFF)),
+        'background' => '#' . dechex(mt_rand(0xBA, 0xFF)) . dechex(mt_rand(0xBA, 0xFF)) . dechex(mt_rand(0xBA, 0xFF)),
         'version' => array_find(
             json_decode(file_get_contents(__DIR__ . '/../composer.lock'), true)['packages'],
             fn (array $package) => $package['name'] === 'robertwesner/simple-mvc-php',
